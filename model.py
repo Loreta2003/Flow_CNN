e95fe9709bcbae35503280f66ab18a2543e7f7c7
Loreta <36799053+Loreta2003@users.noreply.github.com>
2024-10-13 16:10:04 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+dim = 64
+input_shape = (None, dim, dim, 3) 
+sequence_length = 10 
+
+def build_cnn_predictor(input_shape):
+    model = models.Sequential()
+    
+    model.add(layers.Conv3D(dim, (3, 3, 3), activation='relu', input_shape=input_shape, padding='same'))
+    model.add(layers.MaxPooling3D((1, 2, 2), padding='same'))
+    
+    model.add(layers.Conv3D(128, (3, 3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling3D((1, 2, 2), padding='same'))
+    
+    model.add(layers.Conv3D(256, (3, 3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling3D((1, 2, 2), padding='same'))
+    
+    model.add(layers.Conv3D(512, (3, 3, 3), activation='relu', padding='same'))
+    
+    model.add(layers.Flatten())
+    
+    model.add(layers.Dense(1024, activation='relu'))
+    model.add(layers.Dense(dim * dim * 3, activation='sigmoid')) 
+    
+    model.add(layers.Reshape((dim, dim, 3)))
+
+    return model
