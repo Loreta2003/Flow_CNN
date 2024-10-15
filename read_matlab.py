e1dc763a9a426672ebe583aed48e60ef12e7ec5c
Loreta <36799053+Loreta2003@users.noreply.github.com>
2024-10-15 18:49:46 -0400

    Update read_matlab.py

--- read_matlab.py
+++ read_matlab.py
@@ -6,7 +6,7 @@ with h5py.File('minimal_2D_UVW_yplus_20.mat', 'r') as file:
     U_data = file['U_2D'][:]
     V_data = file['V_2D'][:]
     W_data = file['W_2D'][:]
-    for seq_len in range(100):
+    for seq_len in range(9000):
         dataset.append([])
         for x_coord in range(64):
             dataset[seq_len].append([])
